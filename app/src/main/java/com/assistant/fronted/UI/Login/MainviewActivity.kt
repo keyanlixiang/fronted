d4e444ed09eac09308e58c4fa7f0f3752e751a1b
Noble047 <2043915929@qq.com>
2022-08-15 09:43:35 +0800

    8_15:添加预计需要的依赖

--- app/src/main/java/com/assistant/fronted/UI/Login/MainviewActivity.kt
+++ app/src/main/java/com/assistant/fronted/UI/Login/MainviewActivity.kt
@@ -3,10 +3,14 @@ package com.assistant.fronted.UI.Login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.assistant.fronted.R
+import com.assistant.fronted.databinding.ActivityMainviewBinding
 
 class MainviewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mainview)
+        binding = ActivityMainviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

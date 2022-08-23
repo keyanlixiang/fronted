661671b59492989e454a214ce24e1ad2499d2e99
Noble047 <2043915929@qq.com>
2022-08-23 17:31:39 +0800

    8:23:清空loginviewmodel

--- app/src/main/java/com/assistant/fronted/UI/Login/LoginViewModel.kt
+++ app/src/main/java/com/assistant/fronted/UI/Login/LoginViewModel.kt
@@ -1,12 +1,6 @@
 package com.assistant.fronted.UI.Login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import com.assistant.fronted.UI.Faculty.User
-import com.assistant.fronted.model.Faculty
-import com.assistant.fronted.network.LoginNetwork
 
 class LoginViewModel: ViewModel() {
 

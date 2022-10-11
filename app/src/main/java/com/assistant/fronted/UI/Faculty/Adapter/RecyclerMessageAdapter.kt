5efe0f7983776c8dc86b9ea72658cbf2c5a385e3
Noble047 <2043915929@qq.com>
2022-10-11 20:12:27 +0800

    10_11:些许更改

---
+++ app/src/main/java/com/assistant/fronted/UI/Faculty/Adapter/RecyclerMessageAdapter.kt
@@ -0,0 +1,7 @@
+package com.assistant.fronted.UI.Faculty.Adapter
+
+/**
+ * 通知显示的Adapter
+ */
+class RecyclerMessageAdapter {
+}
--- app/src/main/java/com/assistant/fronted/UI/Faculty/Fragments/NotificationFragment.kt
+++ app/src/main/java/com/assistant/fronted/UI/Faculty/Fragments/NotificationFragment.kt
@@ -6,10 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.assistant.fronted.R
+import com.assistant.fronted.databinding.FragmentNotificationBinding
 
 class NotificationFragment : Fragment() {
 
+    lateinit var binding: FragmentNotificationBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+        binding = FragmentNotificationBinding.inflate(layoutInflater)
+
+
+        return binding.root
     }
 }
